70525116d06e5010d4ed3b62a72ecc2404919552
Kristofferjoha <kristofferjoha@gmail.com>
2025-09-20 13:33:19 +0200

    engine tests

--- src/engine.rs
+++ src/engine.rs
@@ -54,3 +54,48 @@ impl MatchingEngine {
         self.books.get(instrument).map(|book| book.display())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::order::{Order};
+    use crate::utils::{Side, OrderType};
+    use crate::utils::MatchingEngineError;
+    use rust_decimal_macros::dec;
+
+    #[test]
+    fn test_add_and_has_market() {
+        let mut engine = MatchingEngine::new();
+        assert!(!engine.has_market("SOFI"));
+        
+        engine.add_market("SOFI".to_string());
+        assert!(engine.has_market("SOFI"));
+    }
+
+    #[test]
+    fn test_process_order_for_non_existent_market() {
+        let mut engine = MatchingEngine::new();
+        let order = Order::new_limit("NON-EXISTENT".to_string(), Side::Buy, dec!(100.0), dec!(10));
+        
+        let result = engine.process_order(order);
+
+        assert!(result.is_err());
+        matches!(result.unwrap_err(), MatchingEngineError::MarketNotFound(market) if market == "NON-EXISTENT");
+    }
+
+    #[test]
+    fn test_process_order_invalid_price_rules() {
+        let mut engine = MatchingEngine::new();
+        engine.add_market("SOFI".to_string());
+
+        let mut limit_no_price = Order::new_market("SOFI".to_string(), Side::Buy, dec!(10));
+        limit_no_price.order_type = OrderType::Limit;
+        let res1 = engine.process_order(limit_no_price);
+        assert!(matches!(res1.unwrap_err(), MatchingEngineError::InvalidOrderPrice));
+
+        let mut market_with_price = Order::new_limit("SOFI".to_string(), Side::Buy, dec!(100.0), dec!(10));
+        market_with_price.order_type = OrderType::Market;
+        let res2 = engine.process_order(market_with_price);
+        assert!(matches!(res2.unwrap_err(), MatchingEngineError::InvalidOrderPrice));
+    }
+}
